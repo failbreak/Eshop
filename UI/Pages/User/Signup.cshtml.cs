1f0b814a94daae395688782dd15880b6fce769a0
Empire Works <Empire Works@DESKTOP-U7R6FSG>
2023-04-07 13:34:00 +0200

    hello

--- UI/Pages/User/Signup.cshtml.cs
+++ UI/Pages/User/Signup.cshtml.cs
@@ -1,11 +1,18 @@
+using DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ServiceLayer.Service;
 
 namespace UI.Pages.User
 {
     public class SignupModel : PageModel
     {
+        private readonly CustomerService _customerService;
+        public SignupModel(CustomerService customerService) => _customerService = customerService;
+
+
+        public Product Product { get; set; }
         public void OnGet()
         {
         }
