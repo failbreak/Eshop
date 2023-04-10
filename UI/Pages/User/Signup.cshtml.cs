f6dda02f9ebad64c78d100cdfd14483ab4e40d52
Empire Works <Empire Works@DESKTOP-U7R6FSG>
2023-04-10 23:07:26 +0200

    help me

--- UI/Pages/User/Signup.cshtml.cs
+++ UI/Pages/User/Signup.cshtml.cs
@@ -1,6 +1,7 @@
 using DataLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Build.Framework;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using ServiceLayer.Service;
 
@@ -11,10 +12,14 @@ namespace UI.Pages.User
         private readonly ICustomerService _customerService;
         public SignupModel(ICustomerService customerService) => _customerService = customerService;
 
+        [BindProperty, Required]
         public string FirstName { get; set; }
+        [BindProperty, Required]
         public string LastName { get; set; }
+        [BindProperty, Required]
         public string Address { get; set; }
         public string Password { get; set; }
+        [BindProperty, Required]
         public string Email { get; set; }
         public List<Customer> customers { get; set; }
         public async Task OnGetAsync()
