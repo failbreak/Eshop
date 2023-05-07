ba999fba4efe51f3766e4f1c984d0442cb77701b
Empire Works <Empire Works@DESKTOP-U7R6FSG>
2023-05-07 18:52:11 +0200

    another fix

--- ServiceLayer/Service/IProductService.cs
+++ ServiceLayer/Service/IProductService.cs
@@ -1,19 +1,18 @@
 ﻿using DataLayer.Entities;
-using ServiceLayer.Service.Dto;
 using ServiceLayer.Service.DtoSorts;
 
 namespace ServiceLayer.Service
 {
     public interface IProductService
     {
-        IQueryable<ProductDto> AddProduct();
-        ProductDto Create(ProductDto newProdukt);
-        ProductDto Delete(int productId);
+        IQueryable<Product> AddProduct();
+        Task<Product> Create(Product newProdukt);
+        Task<Product> DeleteProduct(int productId);
         IQueryable<Category> GetCategories();
         IQueryable<Manufacture> GetManufactuers();
-        IQueryable<ProductDto> GetProducts();
-        ProductDto GetProductById(int productId);
-        IQueryable<ProductDto> SortFilterPage(SortFilterOptions options);
-        ProductDto Update(ProductDto updateProduct);
+        IQueryable<Product> GetProductById(int productId);
+        IQueryable<Product> GetProducts();
+        IQueryable<Product> SortFilterPage(SortFilterOptions options);
+        Task<Product> Update(Product updateProduct);
     }
 }
