069dc1fa2773a656dda8c36b3d321b8125c9c235
Empire Works <Empire Works@DESKTOP-U7R6FSG>
2023-05-08 02:42:42 +0200

    ill me

---
+++ UiBlazor/Models/ManufacturerModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UiBlazor.Models
+{
+    public class Manufacturer
+    {
+        [Required]
+        public int? ManufacturerId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+    }
+}
